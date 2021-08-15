4d4997bd9a2539075b1e1f4df03042ab0409f7a3
Keith Hall <hkeith62@gmail.com>
2021-08-15 10:16:06 -0500

    Added comments and attribution.

--- week-1/hall-modules.js
+++ week-1/hall-modules.js
@@ -1,6 +1,16 @@
-var url = require("url");
-var parsedURL = url.parse("https://github.com/profile?name=hall");
-console.log(parsedURL.protocol);
+/**
+;   Title: WEB 340 –  Node.js
+;   Author: Professor Krasso
+;   Date: 05/30/2021
+;   Modified By: Keith Hall
+;   Description: This is an example of how to require modules/packages in Node.
+*/
+var url = require("url"); /** This takes the name of the module as a string argument and returns the module as an object. 
+                           ;  The object with the parse function attached is put inside the url variable.
+						  */
+var parsedURL = url.parse("https://github.com/profile?name=hall"); // Uses the parse function of the module and puts the result in the parsedURL variable.
+// Use parsedURL to display data from the url module.
+console.log(parsedURL.protocol); 
 console.log(parsedURL.host);
 console.log(parsedURL.query);
 console.log(parsedURL.path);
