20f3186c5d5c0e398a5d847343cad78aea0297e2
Keith Hall <hkeith62@gmail.com>
2021-08-27 10:56:45 -0500

    Initial submission of exercise 3.3

--- week-3/advanced-routing/hall-exercise-3.3.js
+++ week-3/advanced-routing/hall-exercise-3.3.js
@@ -3,7 +3,7 @@
 ;   Author: Professor Krasso
 ;   Date: 08/24/2021
 ;   Modified By: Keith Hall
-;   Description: This is an example of how to set up and use the Mogan logging module.
+;   Description: This is an example of hadvanced routing in express.
 */
 // Modules to be used in the project.
 var express = require("express");
@@ -11,24 +11,23 @@ var http = require("http");
 var path = require("path");
 var logger = require("morgan");
 
-var app = express();   // creates an express application and puts it inside the app variable.
+var app = express(); // creates an express application and puts it inside the app variable.
 
 app.set("views", path.resolve(__dirname, "views")); // Tells Express that the 'views files are in the views directory.
 app.set("view engine", "ejs"); // Tells Express to use the EJS view engine.
 
-app.use(logger("short")); // Uses morgan middleware instead of Node.
+app.use(logger("short")); // Tell Node to use morgan middleware.
 
-app.get("/",function(request, response) {
-	
-	response.render("index.ejs",{
-		
-	message: "This page is an example of the Morgan logging module for Web-340 Exercise 3.2"
-    });
+app.get(/(\d+)$/, function(request, response) {
+	var employeeId = parseInt(request.params[0], 10);
+	response.render("index", {
+		employeeId: employeeId
+	});
 });
 
 http.createServer(app).listen(8080, function() {
-	
-	console.log("Application started on port 8080");
+	console.log("Aplication started on port 8080");
 });
-	
-	
+
+
+
