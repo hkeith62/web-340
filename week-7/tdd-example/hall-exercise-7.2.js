758b29f65268c392b27511717473064675daa713
KEITH HALL <keith@khprojectmanager.com>
2021-09-26 16:07:52 -0500

    Update hall-exercise-7.2.js

--- week-7/tdd-example/hall-exercise-7.2.js
+++ week-7/tdd-example/hall-exercise-7.2.js
@@ -3,7 +3,7 @@
 ;   Author: Professor Krasso
 ;   Date: 09/21/2021
 ;   Modified By: Keith Hall
-;   Description: This is an example of the code/function that can be written after test passed- TDD example, exercise 7.2. 
+;   Description: This is an example of the code/function that can be written if test passes- TDD example, exercise 7.2. 
 */
 function getFruits(str) {
     return str.split(','); // Specific code (str.split) tested.
