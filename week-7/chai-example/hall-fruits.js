1b1d47441cda9cbfe148eddc40160af39d282bdf
KEITH HALL <keith@khprojectmanager.com>
2021-09-26 16:16:53 -0500

    Update hall-fruits.js

--- week-7/chai-example/hall-fruits.js
+++ week-7/chai-example/hall-fruits.js
@@ -5,7 +5,7 @@
 ;   Modified By: Keith Hall
 ;   Description: This is a file containing the code/function to be tested in exercise 7.3.  
 */
-// Function that splits a comma separated string into an array.   
+// Function to be tested.  
 function fruits(str) {
     return str.split(',');     // Tests if str('apple, orange, mango').split returns an array of fruits.
    }
