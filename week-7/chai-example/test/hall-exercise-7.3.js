7fefe0325edae7ed22f706118b9e9258ac5221f2
KEITH HALL <keith@khprojectmanager.com>
2021-09-26 16:19:04 -0500

    Update hall-exercise-7.3.js

--- week-7/chai-example/test/hall-exercise-7.3.js
+++ week-7/chai-example/test/hall-exercise-7.3.js
@@ -5,9 +5,9 @@
 ;   Modified By: Keith Hall
 ;   Description: This is an example using Mocha and Chai frameworks to test a function.  
 */
-var fruits = require("../hall-fruits");   // Requires the module that contains the function to be tested.
-var chai = require("chai");            // Requires chai.
-var assert = chai.assert;             // Chai's assert property is stored in the assert variable.
+var fruits = require("../hall-fruits");   // Requires the module/file that contains the function to be tested.
+var chai = require("chai");              // Requires chai.
+var assert = chai.assert;               // Chai's assert property is stored in the assert variable.
 
 describe("fruits", function() {
     it("should return an array of fruits", function() {
