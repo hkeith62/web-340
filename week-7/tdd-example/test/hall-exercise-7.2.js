4032421e7cf646723290613a4c71960bfbdae014
Keith Hall <hkeith62@gmail.com>
2021-09-26 14:44:08 -0500

    Review and final submission of exercise 7.2.

--- week-7/tdd-example/test/hall-exercise-7.2.js
+++ week-7/tdd-example/test/hall-exercise-7.2.js
@@ -3,13 +3,14 @@
 ;   Author: Professor Krasso
 ;   Date: 09/21/2021
 ;   Modified By: Keith Hall
-;   Description: This is an example using Node/Mocha in TDD- exercise 7.2. 
+;   Description: This is an example of a simple TDD test case using Node- exercise 7.2. 
 */
 var assert = require("assert"); // Defines if a given expression is true or not.
- 
-describe("String#split", function() { 
+
+//Function tested 
+describe("String#split", function() {  
     it("should return an array of fruits", function() {
-        assert(Array.isArray('Apple, Orange, Mango'.split(','))); // Tests if .split (',') returns an array of fruits.
+        assert(Array.isArray('Apple, Orange, Mango'.split(','))); // Tests if string (apple, orange, mago).split (',') returns an array of fruits.
     });
 });
  
